894bf521d883b926ed42963bae2c8ffffe8547ee
Ojvar <MOjvar.AmirHossein@Gmail.com>
2021-11-12 11:22:13 +0330

    Modify headers middleware

--- src/server.js
+++ src/server.js
@@ -11,13 +11,32 @@ app.use("/resources", express.static(path.resolve("resource")));
 app.set("view engine", "pug");
 // app.set("views", path.resolve("views"));
 
+app.use((req, res, next) => {
+    res.setHeader("X-Powered-By", "Meysam Karimi");
+    next();
+
+    // req.rawBody = "";
+    // req.setEncoding("utf8");
+    // req.on("data", function (chunk) {
+    //     req.rawBody += chunk;
+    // });
+    // req.on("end", function () {
+    //     if(req.headers['content-type'] == "application/json") {
+    //         req.body = JSON.parse(req.rawBody);
+    //     }
+    //     console.log(req.rawBody)
+    //     console.log(req.body)
+    //     next();
+    // });
+});
+
 /* Setup */
-app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-);
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    next();
+});
 
 /* Define routes */
 app.use(require("./routes"));
@@ -25,6 +44,6 @@ app.use(require("./routes"));
 /* Start Listening */
 app.listen(ServerConfig.PORT, () => {
     console.log(
-        "Server is initalized and listening on port " + ServerConfig.PORT,
+        "Server is initalized and listening on port " + ServerConfig.PORT
     );
 });
