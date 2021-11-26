2be08e1a5b4fbe6eec9cfe34e6f9fa8bb9bb7b6f
Ojvar <MOjvar.AmirHossein@Gmail.com>
2021-11-26 18:28:51 +0330

    Add vuejs to index page

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,47 +1,61 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 module.exports = router;
 
 /**
+ * Books list
+ */
+router.get('/api/v1/books', (req, res, next) => {
+  setTimeout(() => {
+    const data = [
+      { title: 'T1', publisher: 'P1' },
+      { title: 'T2', publisher: 'P2' },
+    ];
+
+    res.send(data);
+  }, 2000);
+});
+
+/**
  * About page
  */
-router.get("/about", (req, res, next) => {
-    res.render("about.pug");
+router.get('/about', (req, res, next) => {
+  res.render('about.pug');
 });
 
 /**
  * Contact us page
  */
-router.get("/contact-us", (req, res, next) => {
-    res.render("contact-us.pug");
+router.get('/contact-us', (req, res, next) => {
+  res.render('contact-us.pug');
 });
-router.post("/contact-us", (req, res, next) => {
-    const { email, comment } = req.body;
+router.post('/contact-us', (req, res, next) => {
+  const { email, comment } = req.body;
 
-    /* Validate user data */
-    if (email == undefined || comment == undefined) {
-        res.sendStatus(400);
-        return;
-    }
+  /* Validate user data */
+  if (email == undefined || comment == undefined) {
+    res.sendStatus(400);
+    return;
+  }
 
-    /* Send response */
-    const message = `Dear ${email} <br/>
+  /* Send response */
+  const message = `Dear ${email} <br/>
     Your comment is admitted <br/>
     We appreciate it ;)`;
 
-    res.send({
-        message,
-        comment,
-    });
+  res.send({
+    message,
+    comment,
+  });
 });
 
 /**
  * Index page
  */
-router.get("/:username?", (req, res, next) => {
-    res.render("index.pug", {
-        username: req.params.username || "My Dear Visitor",
-    });
+router.get('/:username?', (req, res, next) => {
+  res.render('index.pug', {
+    username: req.params.username || 'My Dear Visitor',
+  });
 });
