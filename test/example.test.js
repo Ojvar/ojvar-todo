19874d6c4c8b7d8b9f6a0087e242cb436ef2b70c
Ojvar <MOjvar.AmirHossein@Gmail.com>
2021-12-03 10:39:38 +0330

    Update example test
    Add incorrect test function

--- test/example.test.js
+++ test/example.test.js
@@ -1,13 +1,24 @@
 describe("Sum numbers", () => {
   test("it should sum two numbers correctly", sumTest);
+  test("it should sum two numbers incorrectly", sumFailedTest);
 });
 
 /**
  * Test sum function
  */
 function sumTest() {
+  const sum = 2 + 2;
+  const expectedResult = 4;
+
+  expect(sum).toEqual(expectedResult);
+}
+
+/**
+ * Test sum-failed function
+ */
+function sumFailedTest() {
   const sum = 1 + 2;
-  const expectedResult = 3;
+  const expectedResult = 4;
 
   expect(sum).toEqual(expectedResult);
 }
